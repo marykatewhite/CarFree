cf97a06071ffc01e0f220722c4d25d541c29644e
MaryEtak <marykatewhite@gmail.com>
2020-01-02 12:31:52 -0500

    tweaks

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('mapDisplay'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 37.540, lng: -77.434},
+    zoom: 13
   });
 }
 
