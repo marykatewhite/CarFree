2e71a3bdef9baaea0576dad9154f58045ea9f049
zsadov1 <zsadov1@gmail.com>
2020-01-02 17:42:28 -0500

    Added Current location to java script

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
-var map;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(displayLocationInfo);
+}
+
+function displayLocationInfo(position) {
+  const long = position.coords.longitude;
+  const lati = position.coords.latitude;
+
+  
+
+  console.log(`longitude: ${ long } | latitude: ${ lati }`);
+}
+
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('mapDisplay'), {
-    center: {lat: 37.540, lng: -77.434},
-    zoom: 13
+    center: {lat: 41.850033, lng: -87.6500523},
+    zoom: 3
   });
 }
 
