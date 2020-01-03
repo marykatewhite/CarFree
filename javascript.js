a3f55223780d7247a8dbfb27cecabf39a92bec64
zsadov1 <zsadov1@gmail.com>
2020-01-03 13:24:59 -0500

    Mapstyle JSON and updated Javascript.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,223 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(displayLocationInfo);
+
+
+// var map, infoWindow;
+//       function initMap() {
+//         map = new google.maps.Map(document.getElementById('mapDisplay'), {
+//           center: {lat: 41.850033, lng: -87.6500523},
+//           zoom: 8
+//         });
+//         infoWindow = new google.maps.InfoWindow;
+
+//         // Try HTML5 geolocation.
+//         if (navigator.geolocation) {
+//           navigator.geolocation.getCurrentPosition(function(position) {
+//             var pos = {
+//               lat: position.coords.latitude,
+//               lng: position.coords.longitude
+//             };
+
+//             infoWindow.setPosition(pos);
+//             infoWindow.setContent('You are Here!');
+//             infoWindow.open(map);
+//             map.setCenter(pos);
+//           }, function() {
+//             handleLocationError(true, infoWindow, map.getCenter());
+//           });
+//         } else {
+//           // Browser doesn't support Geolocation
+//           handleLocationError(false, infoWindow, map.getCenter());
+//         }
+//       }
+
+//       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//         infoWindow.setPosition(pos);
+//         infoWindow.setContent(browserHasGeolocation ?
+//                               'Error: The Geolocation service failed.' :
+//                               'Error: Your browser doesn\'t support geolocation.');
+//         infoWindow.open(map);
+//       }
+
+    
+
+  var map, infoWindow;
+
+  function createMap(){
+    var options = {
+      center: {lat: 41.850033, lng: -87.6500523},
+      zoom: 13,
+      styles: [
+          {elementType: "geometry", stylers: [{color: "#ebe3cd"}]},
+          {elementType: "labels.text.fill", stylers: [{color: "#523735"}]},
+          {elementType: "labels.text.stroke", stylers: [{color: "#f5f1e6"}]},
+          {
+            featureType: "administrative",
+            elementType: "geometry.stroke",
+            stylers: [{color: "#c9b2a6"}]
+          },
+          {
+            featureType: "administrative.land_parcel",
+            elementType: "geometry.stroke",
+            stylers: [{color: "#dcd2be"}]
+          },
+          {
+            featureType: "administrative.land_parcel",
+            elementType: "labels.text.fill",
+            stylers: [{color: "#ae9e90"}]
+          },
+          {
+            featureType: "landscape.natural",
+            elementType: "geometry",
+            stylers: [{color: "#dfd2ae"}]
+          },
+          {
+            featureType: "poi",
+            elementType: "geometry",
+            stylers: [{color: "#dfd2ae"}]
+          },
+          {
+            featureType: "poi",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#93817c"}]
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry.fill",
+            stylers: [{color: "#a5b076"}]
+          },
+          {
+            featureType: "poi.park",
+            elementType: "labels.text.fill",
+            stylers: [{color: "#447530"}]
+          },
+          {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [{color: "#f5f1e6"}]
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [{color: "#fdfcf8"}]
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry",
+            stylers: [{color: "#f8c967"}]
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [{color: "#e9bc62"}]
+          },
+          {
+            featureType: "road.highway.controlled_access",
+            elementType: "geometry",
+            stylers: [{color: "#e98d58"}]
+          },
+          {
+            featureType: "road.highway.controlled_access",
+            elementType: "geometry.stroke",
+            stylers: [{color: "#db8555"}]
+          },
+          {
+            featureType: "road.local",
+            elementType: "labels.text.fill",
+            stylers: [{color: "#806b63"}]
+          },
+          {
+            featureType: "transit.line",
+            elementType: "geometry",
+            stylers: [{color: "#dfd2ae"}]
+          },
+          {
+            featureType: "transit.line",
+            elementType: "labels.text.fill",
+            stylers: [{color: "#8f7d77" }]
+          },
+          {
+            featureType: "transit.line",
+            elementType: "labels.text.stroke",
+            stylers: [{color: "#ebe3cd"}]
+          },
+          {
+            featureType: "transit.station",
+            elementType: "geometry",
+            stylers: [{color: "#dfd2ae"}]
+          },
+          {
+            featureType: "water",
+            elementType: "geometry.fill",
+            stylers: [{color: "#b9d3c2"}]
+          },
+          {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [{color: "#92998d"}]
+        },
+        ]
+    };
+
+    map = new google.maps.Map(document.getElementById("mapDisplay"), options);
+
+    var input = document.getElementById("inputDestination");
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    map.addListener("bound_changed" , function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    searchBox.addListener('places_changed' , function(){
+      var places = seachbox.getPlaces();
+
+      if (places.length === 0) 
+        return;
+
+    markers.forEach(function(m) { m.setMap(null); });
+    markers = [];
+
+    var bounds = new google.maps.LatLngBounds();
+
+    places.forEach(function (p) {
+      if (lp.geometery)
+        return;
+
+      markers.push(new google.maps.Marker({
+        map: map,
+        title: p.name,
+        position: p.geometery.location 
+      }));
+
+      if (p.geometery.viewport)
+        bounds.union(p.geometery.viewport);
+      else
+        bounds.extend(p.geometery.location);
+    });
+    map.fitBounds(bounds);
+    })
+
+
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (p) {
+        var position = {
+          lat: p.coords.latitude,
+          lng: p.coords.longitude
+        };
+        infoWindow.setPosition(position);
+        infoWindow.setContent("You're here!")
+        infoWindow.open(map);
+      }, function () {
+        handleLocationError('Geolocation service failed', map.center());
+      })
+    } else{
+      handleLocationError('No Geolocation Avaiable' , map.center());
+    }
 }
 
-function displayLocationInfo(position) {
-  const long = position.coords.longitude;
-  const lati = position.coords.latitude;
-
-  
-
-  console.log(`longitude: ${ long } | latitude: ${ lati }`);
+function handleLocationError (content, position){
+  infoWindow.setPosition(position);
+  infoWindow.setContent(content);
+  infoWindow.open(map);
 }
-
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('mapDisplay'), {
-    center: {lat: 41.850033, lng: -87.6500523},
-    zoom: 3
-  });
-}
-
