7d70a27dc907875f7735a78e9aa5d0526597f20a
ljdurante <57123555+ljdurante@users.noreply.github.com>
2020-01-04 09:54:34 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -224,6 +224,12 @@ directionsService.route(directionObject, function(result, status) {
   
 let rainChance = document.querySelector('#rain-chance');
 
+
+    if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(position => {
+         long = position.coords.longitude;
+         lat = position.coords.latitude;
+
            const proxy = `https://cors-anywhere.herokuapp.com/`;
            const api = `${proxy}https://api.darksky.net/forecast/9c3f1b2093600100e3b6d747fa501d3f/${lat},${long}`; 
                 
@@ -235,5 +241,8 @@ let rainChance = document.querySelector('#rain-chance');
                         console.log(data);
                         const {summary} = data.minutely;
                       rainChance.textContent = summary;
-                    });   
+                    });
+
+                 });
+}
 }
